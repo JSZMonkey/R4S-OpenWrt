f1542fb9a983c552e7d025d0c6af24ab99924d35
JSZMonkey <22406031+JSZMonkey@users.noreply.github.com>
2021-09-14 17:35:24 +0800

    Update 01_get_ready.sh

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-latest_release="$(curl -s https://github.com/openwrt/openwrt/releases |grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" |sed -n '/21/p' |sed -n 1p)"
-curl -LO "https://github.com/openwrt/openwrt/archive/${latest_release}"
-mkdir openwrt_back
-shopt -s extglob 
-tar zxvf ${latest_release}  --strip-components 1 -C ./openwrt_back
-rm -f ${latest_release}
+latest_release="$(curl -s https://github.com/openwrt/openwrt/releases |grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" |sed -n '/21/p' |sed -n 1p |sed 's/.tar.gz//g')"
+git clone --single-branch -b ${latest_release} https://github.com/openwrt/openwrt openwrt_back
 git clone --single-branch -b openwrt-21.02 https://github.com/openwrt/openwrt openwrt_new
 rm -f ./openwrt_new/include/version.mk
 rm -f ./openwrt_new/include/kernel-version.mk
