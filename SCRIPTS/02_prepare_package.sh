f5c209c8c9728dbe2e250471e1a9071d9195fefd
JSZMonkey <22406031+JSZMonkey@users.noreply.github.com>
2021-09-17 21:17:44 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -197,7 +197,7 @@ sed -i 's,flowoffload.@flow[0],firewall.@defaults[0],g' package/new/OpenAppFilte
 sed -i 's/+docker/+docker \\\n\t+dockerd/g' ./feeds/luci/applications/luci-app-dockerman/Makefile
 
 # Dnsfilter
-git clone --depth 1 https://github.com/garypang13/luci-app-dnsfilter.git package/new/luci-app-dnsfilter
+git clone --depth 1 https://github.com/kiddin9/luci-app-dnsfilter.git package/new/luci-app-dnsfilter
 
 # Dnsproxy
 svn co https://github.com/immortalwrt/packages/trunk/net/dnsproxy feeds/packages/net/dnsproxy
