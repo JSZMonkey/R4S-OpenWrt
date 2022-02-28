a74070dc31b2174b2b58e143b8665a81248798a2
JSZMonkey <9168131@qq.com>
2022-02-28 17:06:45 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -321,7 +321,7 @@ popd
 
 # v2raya
 git clone --depth 1 https://github.com/zxlhhyccc/luci-app-v2raya.git package/new/luci-app-v2raya
-svn co https://github.com/v2rayA/v2raya-openwrt/trunk/v2raya package/new/v2raya
+svn co https://github.com/fw876/helloworld/trunk/v2raya package/new/v2raya
 
 # 订阅转换
 svn co https://github.com/immortalwrt/packages/trunk/net/subconverter feeds/packages/net/subconverter
