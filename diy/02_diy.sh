5b6a5d7bff175d033d87e1c16010819c182ba8c1
JSZMonkey <22406031+JSZMonkey@users.noreply.github.com>
2023-02-12 16:24:55 +0800

    Update 02_diy.sh

--- diy/02_diy.sh
+++ diy/02_diy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 clear
 
-rm -rf ../PATCH/files-5.10/arch/arm64/boot/dts/rockchip/rk3568-nanopi-r5c.dts
-
 # Add luci-app-eqos
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-eqos package/new/luci-app-eqos
 
