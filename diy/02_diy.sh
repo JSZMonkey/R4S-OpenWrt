886982132d0bd15a42903118eb888ffcc12bb6bb
JSZMonkey <22406031+JSZMonkey@users.noreply.github.com>
2023-03-16 11:12:21 +0800

    Update 02_diy.sh

--- diy/02_diy.sh
+++ diy/02_diy.sh
@@ -9,8 +9,3 @@ rm -rf ./package/new/mosdns
 svn export https://github.com/sbwml/luci-app-mosdns/trunk/mosdns package/new/mosdns
 svn export https://github.com/sbwml/luci-app-mosdns/trunk/luci-app-mosdns package/new/luci-app-mosdns
 
-uci set dhcp.lan.ra='hybrid'
-uci set dhcp.lan.ndp='hybrid'
-uci set dhcp.lan.dhcpv6='hybrid'
-uci set dhcp.lan.ra_management='1'
-uci commit dhcp
