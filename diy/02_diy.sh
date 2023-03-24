4f641ce99611f3bfd64c8c39f82b16a5c5cd15be
JSZMonkey <22406031+JSZMonkey@users.noreply.github.com>
2023-03-24 19:30:08 +0800

    Update 02_diy.sh

--- diy/02_diy.sh
+++ diy/02_diy.sh
@@ -5,5 +5,6 @@ clear
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-eqos package/new/luci-app-eqos
 rm -rf ./package/new/luci-app-mosdns
 rm -rf ./package/new/mosdns
+rm -rf ./package/new/v2ray-geodata
 svn export -r v4 https://github.com/sbwml/luci-app-mosdns/trunk/mosdns package/new/mosdns
 svn export -r v4 https://github.com/sbwml/luci-app-mosdns/trunk/luci-app-mosdns package/new/luci-app-mosdns
