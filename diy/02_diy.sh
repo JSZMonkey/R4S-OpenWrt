70573d4ce79dc6102e250dfa117b3140ffd69b40
JSZMonkey <22406031+JSZMonkey@users.noreply.github.com>
2023-03-24 19:27:08 +0800

    Update 02_diy.sh

--- diy/02_diy.sh
+++ diy/02_diy.sh
@@ -3,3 +3,7 @@ clear
 
 # Add luci-app-eqos
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-eqos package/new/luci-app-eqos
+rm -rf ./package/new/luci-app-mosdns
+rm -rf ./package/new/mosdns
+svn export -r v4 https://github.com/sbwml/luci-app-mosdns/trunk/mosdns package/new/mosdns
+svn export -r v4 https://github.com/sbwml/luci-app-mosdns/trunk/luci-app-mosdns package/new/luci-app-mosdns
