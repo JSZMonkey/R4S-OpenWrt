78264faf402269e0a130a02fcea9879a69b89db6
JSZMonkey <22406031+JSZMonkey@users.noreply.github.com>
2023-10-18 11:36:17 +0800

    Update 02_diy.sh

--- diy/02_diy.sh
+++ diy/02_diy.sh
@@ -4,3 +4,6 @@ clear
 # Add luci-app-eqosplus
 cp -rf ../sirpdboy/luci-app-eqosplus ./package/sirpdboy/luci-app-eqosplus
 
+# Add luci-app-netwizard
+cp -rf ../sirpdboy/luci-app-netwizard ./package/sirpdboy/luci-app-netwizard
+
