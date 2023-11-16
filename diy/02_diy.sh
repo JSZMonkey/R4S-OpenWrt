c217a77d9bb69831044e265806530663707c83e2
JSZMonkey <22406031+JSZMonkey@users.noreply.github.com>
2023-11-16 09:48:08 +0800

    Update 02_diy.sh

--- diy/02_diy.sh
+++ diy/02_diy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 clear
 
-# Add luci-app-eqosplus
-cp -rf ../sirpdboy/luci-app-eqosplus ./package/sirpdboy/luci-app-eqosplus
-
-# Add luci-app-netwizard
-cp -rf ../sirpdboy/luci-app-netwizard ./package/sirpdboy/luci-app-netwizard
+# Add luci-app-eqos
+svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-eqos package/new/luci-app-eqos
 
