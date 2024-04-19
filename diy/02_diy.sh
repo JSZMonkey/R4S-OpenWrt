f863bf49a3c6ffdcc2b38a04dac9aeb94af15b57
JSZMonkey <22406031+JSZMonkey@users.noreply.github.com>
2024-04-19 17:14:06 +0800

    Update 02_diy.sh

--- diy/02_diy.sh
+++ diy/02_diy.sh
@@ -5,4 +5,8 @@ clear
 
 cp -rf ../lede_luci/applications/luci-app-eqos ./package/new/luci-app-eqos
 
-
+# Mosdns
+cp -rf ../lede_pkg/net/mosdns ./package/new/mosdns
+cp -rf ../lede_luci/applications/luci-app-mosdns ./package/new/luci-app-mosdns
+rm -rf ./feeds/packages/net/v2ray-geodata
+cp -rf ../mosdns/v2ray-geodata ./package/new/v2ray-geodata
