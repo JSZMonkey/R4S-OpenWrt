a20e8ff8823b1a896cfc46fa83d700cb5e9bdd38
JSZMonkey <22406031+JSZMonkey@users.noreply.github.com>
2024-04-25 00:13:27 +0800

    Update 02_diy.sh

--- diy/02_diy.sh
+++ diy/02_diy.sh
@@ -13,3 +13,7 @@ cp -rf ../lede_pkg/net/mosdns ./package/new/mosdns
 cp -rf ../lede_pkg/net/v2ray-geodata ./package/new/mosdns
 cp -rf ../lede_pkg/utils/v2dat ./package/new/v2dat
 cp -rf ../lede_luci/applications/luci-app-mosdns ./package/new/luci-app-mosdns
+
+# 流量监视
+rm -rf ./package/new/luci-app-wrtbwmon
+git clone -b patch-1 --depth 1 https://github.com/JSZMonkey/luci-app-wrtbwmon.git package/new/luci-app-wrtbwmon
