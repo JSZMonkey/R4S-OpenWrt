b553cbfa638bcf4c355abbd89e4f848489e6633f
JSZMonkey <22406031+JSZMonkey@users.noreply.github.com>
2024-04-25 00:03:50 +0800

    Update 02_diy.sh

--- diy/02_diy.sh
+++ diy/02_diy.sh
@@ -15,11 +15,5 @@ cp -rf ../lede_pkg/utils/v2dat ./package/new/v2dat
 cp -rf ../lede_luci/applications/luci-app-mosdns ./package/new/luci-app-mosdns
 
 # MACFilter
-#rm -rf ./feeds/luci/applications/luci-app-accesscontrol
 cp -rf ../lede_luci/applications/luci-app-accesscontrol ./package/new/luci-app-accesscontrol
 
-# luci-app-wrtbwmon
-rm -rf ./feeds/luci/applications/luci-app-wrtbwmon
-git clone https://github.com/brvphoenix/luci-app-wrtbwmon -b master ./feeds/luci/applications/luci-app-wrtbwmon
-mv ./feeds/luci/applications/luci-app-wrtbwmon/luci-app-wrtbwmon/* ./feeds/luci/applications/luci-app-wrtbwmon
-rm -rf ./feeds/luci/applications/luci-app-wrtbwmon/luci-app-wrtbwmon
