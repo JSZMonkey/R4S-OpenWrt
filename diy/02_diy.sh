7c6f1e27351bd1c6e9073b399b576f403d91fab5
JSZMonkey <22406031+JSZMonkey@users.noreply.github.com>
2024-07-11 09:17:16 +0800

    Update 02_diy.sh

--- diy/02_diy.sh
+++ diy/02_diy.sh
@@ -16,7 +16,7 @@ rm -rf ./package/new/mosdns
 rm -rf ./package/new/luci-app-mosdns
 rm -rf ./package/new/v2ray-geodata
 cp -rf ../lede_pkg/net/mosdns ./package/new/mosdns
-cp -rf ../lede_pkg/net/v2ray-geodata ./package/new/mosdns
+cp -rf ../lede_pkg/net/v2ray-geodata ./package/new/v2ray-geodata
 cp -rf ../lede_pkg/utils/v2dat ./package/new/v2dat
 cp -rf ../lede_luci/applications/luci-app-mosdns ./package/new/luci-app-mosdns
 
