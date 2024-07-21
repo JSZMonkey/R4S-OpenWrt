7932191322fa853e7bb1d8f6cf0e37f9c84e3042
JSZMonkey <22406031+JSZMonkey@users.noreply.github.com>
2024-07-21 17:12:15 +0800

    Update 02_diy.sh

--- diy/02_diy.sh
+++ diy/02_diy.sh
@@ -12,13 +12,13 @@ cp -rf ../lede_luci/applications/luci-app-nft-qos ./package/new/luci-app-nft-qos
 git clone -b master --depth 1 https://github.com/JSZMonkey/luci-app-control-webrestriction.git package/new/luci-app-control-webrestriction
 
 # Mosdns
-rm -rf ./package/new/mosdns
-rm -rf ./package/new/luci-app-mosdns
-rm -rf ./package/new/v2ray-geodata
-cp -rf ../lede_pkg/net/mosdns ./package/new/mosdns
-cp -rf ../lede_pkg/net/v2ray-geodata ./package/new/v2ray-geodata
-cp -rf ../lede_pkg/utils/v2dat ./package/new/v2dat
-cp -rf ../lede_luci/applications/luci-app-mosdns ./package/new/luci-app-mosdns
+#rm -rf ./package/new/mosdns
+#rm -rf ./package/new/luci-app-mosdns
+#rm -rf ./package/new/v2ray-geodata
+#cp -rf ../lede_pkg/net/mosdns ./package/new/mosdns
+#cp -rf ../lede_pkg/net/v2ray-geodata ./package/new/v2ray-geodata
+#cp -rf ../lede_pkg/utils/v2dat ./package/new/v2dat
+#cp -rf ../lede_luci/applications/luci-app-mosdns ./package/new/luci-app-mosdns
 
 # 流量监视
 rm -rf ./package/new/luci-app-wrtbwmon
