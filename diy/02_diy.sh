e39d06607a4ffca0b56261c51805b886e3d722d2
JSZMonkey <22406031+JSZMonkey@users.noreply.github.com>
2024-10-16 17:34:37 +0800

    Update 02_diy.sh

--- diy/02_diy.sh
+++ diy/02_diy.sh
@@ -5,12 +5,12 @@ clear
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
 # qBittorrent 下载
-svn checkout https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-qbittorrent ./package/new/luci-app-qbittorrent
-svn checkout https://github.com/coolsnowwolf/packages/trunk/net/qBittorrent-static ./package/new/qBittorrent-static
-svn checkout https://github.com/coolsnowwolf/packages/trunk/net/qBittorrent ./package/new/qBittorrent
-svn checkout https://github.com/coolsnowwolf/packages/trunk/libs/qtbase ./package/new/qtbase
-svn checkout https://github.com/coolsnowwolf/packages/trunk/libs/qttools ./package/new/qttools
-svn checkout https://github.com/coolsnowwolf/packages/trunk/libs/rblibtorrent ./package/new/rblibtorrent
+svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-qbittorrent package/new/luci-app-qbittorrent
+svn export https://github.com/coolsnowwolf/packages/trunk/net/qBittorrent-static package/new/qBittorrent-static
+svn export https://github.com/coolsnowwolf/packages/trunk/net/qBittorrent package/new/qBittorrent
+svn export https://github.com/coolsnowwolf/packages/trunk/libs/qtbase package/new/qtbase
+svn export https://github.com/coolsnowwolf/packages/trunk/libs/qttools package/new/qttools
+svn export https://github.com/coolsnowwolf/packages/trunk/libs/rblibtorrent package/new/rblibtorrent
 sed -i 's,"nas","services",g' package/new/luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
 sed -i 's,nas,services,g' package/new/luci-app-qbittorrent/luasrc/view/qbittorrent_status.htm
 # Add luci-app-eqos
