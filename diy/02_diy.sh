f71a015208f069a3c87c744769b6d6f5789120be
JSZMonkey <22406031+JSZMonkey@users.noreply.github.com>
2024-10-18 15:13:26 +0800

    Update 02_diy.sh

--- diy/02_diy.sh
+++ diy/02_diy.sh
@@ -5,14 +5,7 @@ clear
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
 # qBittorrent 下载
-svn co https://github.com/coolsnowwolf/luci/tree/master/applications/luci-app-qbittorrent package/new/luci-app-qbittorrent
-svn co https://github.com/coolsnowwolf/packages/tree/master/net/qBittorrent-static package/new/qBittorrent-static
-svn co https://github.com/coolsnowwolf/packages/tree/master/net/qBittorrent package/new/qBittorrent
-svn co https://github.com/coolsnowwolf/packages/tree/master/libs/qtbase package/new/qtbase
-svn co https://github.com/coolsnowwolf/packages/tree/master/libs/qttools package/new/qttools
-svn co https://github.com/coolsnowwolf/packages/tree/master/libs/rblibtorrent package/new/rblibtorrent
-sed -i 's,"nas","services",g' package/new/luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
-sed -i 's,nas,services,g' package/new/luci-app-qbittorrent/luasrc/view/qbittorrent_status.htm
+git clone -b main --depth 1 https://github.com/JSZMonkey/luci-app-qbittorrent package/new/luci-app-qbittorrent
 
 # Add luci-app-eqos
 
